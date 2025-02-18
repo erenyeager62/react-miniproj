73d9ce25406a250154fa090c035ddcde7142a74e
sarvesh patil <patilsarvesh62@gmail.com>
2025-02-18 08:13:41 +0530

    bgcolorchanger

---
+++ 03bgchanger/src/App.tsx
@@ -0,0 +1,45 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color,setColor] = useState<string>("olive")
+
+  return (
+  
+     <div className="w-screen h-screen duration-200"
+       style={{backgroundColor:color}}
+     >
+       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'>
+           <button onClick={()=>setColor("red")} className='outline-none px-4 py-1 rounded-full test-white shadow-lg' style={{backgroundColor:"red"}}>
+             red
+           </button>
+           <button  onClick={()=>setColor("green")} className='outline-none px-4 py-1 rounded-full test-white shadow-lg' style={{backgroundColor:"green"}}>
+             green
+           </button > 
+           <button onClick={()=>setColor("blue")} className='outline-none px-4 py-1 rounded-full test-white shadow-lg' style={{backgroundColor:"blue"}}>
+             blue
+           </button>
+            <button className='outline-none px-4 py-1 rounded-full test-white shadow-lg' style={{backgroundColor:"red"}}>
+             red
+           </button> 
+           <button className='outline-none px-4 py-1 rounded-full test-white shadow-lg' style={{backgroundColor:"red"}}>
+             red
+           </button> 
+           <button className='outline-none px-4 py-1 rounded-full test-white shadow-lg' style={{backgroundColor:"red"}}>
+             red
+           </button> 
+           <button className='outline-none px-4 py-1 rounded-full test-white shadow-lg' style={{backgroundColor:"red"}}>
+             red
+           </button>
+
+         </div>
+       </div>
+     </div>
+
+    
+  )
+}
+
+export default App
