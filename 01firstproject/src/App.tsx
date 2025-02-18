caaf7f587d4aa2c98b0f6816501db968ca6117d0
sarvesh patil <patilsarvesh62@gmail.com>
2025-02-18 07:13:30 +0530

    added first proj

---
+++ 01firstproject/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>first proj practice</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01firstproject/src/coffee.tsx
@@ -0,0 +1,13 @@
+
+
+const coffee = () => {
+  return (
+    <div>
+       <h1>this is the first proj
+        
+       </h1>
+    </div>
+  )
+}
+
+export default coffee
